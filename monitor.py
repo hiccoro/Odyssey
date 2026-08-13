860a67ad05761f2b5ba0fec2b0b24f728c83e36e
hiccoro <hicro.coc01@gmx.de>
2026-08-13 16:36:50 +0200

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -140,7 +140,7 @@ def save_to_git():
 
 def main():
     check_counter = 1
-    while check_counter <= 3600:
+    while check_counter <= 10000:
         print(f"=== ODYSSEY MONITOR {check_counter} ===")
         now = datetime.now(PRAGUE)
         # Alle jemals bekannten Vorstellungen laden
@@ -150,27 +150,27 @@ def main():
         )
         current = {}
         # Wir prüfen die nächsten 6 Wochen.
-        #for days_ahead in range(42):
-        #    date = now.date() + timedelta(days=days_ahead)
-        #    print(
-        #        f"Prüfe {date}..."
-        #    )
-        #    try:
-        #        events = get_showings_for_date(date)
-        #    except Exception as e:
-        #        print(
-        #            f"Fehler bei {date}: {e}"
-        #        )
-        #        continue
-        #    for event in events:
-        #        event_id = str(event["id"])
-        #        current[event_id] = event
-        #        print(
-        #            event["datetime"],
-        #            "|",
-        #            "available:",
-        #            round(TOTAL_SEATS * event["availabilityRatio"])
-        #        )
+        for days_ahead in range(42):
+            date = now.date() + timedelta(days=days_ahead)
+            print(
+                f"Prüfe {date}..."
+            )
+            try:
+                events = get_showings_for_date(date)
+            except Exception as e:
+                print(
+                    f"Fehler bei {date}: {e}"
+                )
+                continue
+            for event in events:
+                event_id = str(event["id"])
+                current[event_id] = event
+                print(
+                    event["datetime"],
+                    "|",
+                    "available:",
+                    round(TOTAL_SEATS * event["availabilityRatio"])
+                )
         current_ids = set(current.keys())
         # Nur Vorstellungen melden, die noch NIE zuvor
         # in known_showings.json gespeichert wurden.
